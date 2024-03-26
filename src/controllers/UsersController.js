e19fdcbfbac268bc615faaf1b239c45eaaa95f94
Leonardo Menezes <leonardomenezes47@outlook.com>
2024-03-25 23:07:09 -0400

    add jwt again

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -28,11 +28,11 @@ class UsersController {
 
     async update(request, response) {
         const { name, email, password, old_password } = request.body
-        const { id } = request.params
+        const user_id = request.user.id
 
         const database = await sqliteConnection()
         const user = await database.get(
-            "SELECT * FROM users WHERE id = (?)", [id]
+            "SELECT * FROM users WHERE id = (?)", [user_id]
         )
 
         if(!user) {
@@ -71,7 +71,7 @@ class UsersController {
             password = (?),
             updated_at = DATETIME('now')
             WHERE id = (?)`,
-            [user.name, user.email, user.password, id]
+            [user.name, user.email, user.password, user_id]
         )
 
         return response.json()
--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,12 +1,11 @@
 const { Router } = require("express")
 const TagsController = require("../controllers/TagsController")
-
-
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 const TagsRoutes = Router()
 const tagsController = new TagsController()
 
 
-TagsRoutes.get("/:user_id", tagsController.index)
+TagsRoutes.get("/", ensureAuthenticated, tagsController.index)
 
 
 module.exports = TagsRoutes
